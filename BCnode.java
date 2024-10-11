44cad2ebf3b94ef6a22461b23dad6f77da629863
Joe1Ontiveros <jontiverosrodriguez1@gmail.com>
2024-10-11 15:00:37 -0600

    Block done

--- BCnode.java
+++ BCnode.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BCnode {
     private ArrayList<Block> blockchain;
@@ -17,10 +20,14 @@ public class BCnode {
 
     public void initializeBlockchain() {
     	Block b1 = new Block("ABC");
+    	
+    	
+    	
     }
     
     public void addBlock(Block b) {
-        // Implementation
+        
+    	
     }
 
     private void mineBlock(Block b) {
@@ -41,4 +48,64 @@ public class BCnode {
         // Implementation
     	return result;
     }
+    public static void main(String[] args) { // given from project
+        Scanner keyScan = new Scanner(System.in);
+        // Grab my port number on which to start this node
+        System.out.print("Enter port to start (on current IP): ");
+        int myPort = keyScan.nextInt();
+        
+        // Need to get what other Nodes to connect to
+        System.out.print("Enter remote ports (current IP is assumed): ");
+        keyScan.nextLine(); // skip the NL at the end of the previous scan int
+        String line = keyScan.nextLine();
+        List<Integer> remotePorts = new ArrayList<Integer>();
+        if (line != "") {
+            String[] splitLine = line.split(" ");
+            for (int i=0; i<splitLine.length; i++) {
+                remotePorts.add(Integer.parseInt(splitLine[i]));
+            }
+        }
+        // Create the Node
+        BCnode n = new BCnode(myPort, remotePorts);
+        
+        String ip = "";
+        try {
+             ip = Inet4Address.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        
+        System.out.println("Node started on " + ip + ": " + myPort);
+        
+        // Node command line interface
+        while(true) {
+            System.out.println("\nNODE on port: " + myPort);
+            System.out.println("1. Display Node's blockchain");
+            System.out.println("2. Create/mine new Block");
+            System.out.println("3. Kill Node");
+            System.out.print("Enter option: ");
+            int in = keyScan.nextInt();
+            
+            if (in == 1) {
+                System.out.println(n);
+                
+            } else if (in == 2) {
+                // Grab the information to put in the block
+                System.out.print("Enter information for new Block: ");
+                String blockInfo = keyScan.next();
+                Block b = new Block(blockInfo);
+                n.addBlock(b);
+                
+            } else if (in == 3) {
+                // Take down the whole virtual machine (and all the threads)
+                //   for this Node.  If we just let main end, it would leave
+                //   up the Threads the node created.
+                keyScan.close();
+                System.exit(0);
+            }
+        }
+    }
+
+ 
 }
--- Block.java
+++ Block.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.net.Socket;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 
 public class Block {
@@ -12,58 +14,83 @@ public class Block {
     private int nonce;
     private String hash;
     private String previousHash;
-
+    private List<String> hashhistory = new ArrayList<String>();
+ 
     public Block(String data) {
         // Constructor implementation
     	this.data = data;
-    	this.timestamp = System.currentTimeMillis();
-    	this.hash = calculateBlockHash();
+    	this.timestamp = new Date().getTime();
+    	try {
+			this.hash = calculateBlockHash();
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 //    	this.previousHash()
-//    	this.nonce = setNonce(this.hash);
+    	setNonce(0);
+    	
     }
 
     public Block() {
         // Default constructor for Genesis Block
     }
 
-    public String calculateBlockHash() {
-        // Implementation
+    public String calculateBlockHash() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        // condense all vars to one large string
+    	setPreviousHash(this.hash); // save old hash
+    	String combined = this.data + this.timestamp + this.hash;
+    	System.out.printf("Block combined string",combined);
+    	MessageDigest mydigest = MessageDigest.getInstance("SHA-256");
+    	
+    	byte[] hashBytes = mydigest.digest(combined.getBytes("UTF-8"));
+    	
+    	StringBuffer buffer = new StringBuffer();
+    	for (byte b: hashBytes) {
+    	      buffer.append(String.format("%02x", b));
+    	}
+    	String hashStr = buffer.toString();
+    	return hashStr;
+    	
+    	
     }
 
     public String getHash() {
         // Implementation
+    	return this.hash;
     }
 
-    public void setHash(String hash) {
+    public void setHash(String hash) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         // Implementation
+    	this.data = hash;
+    	calculateBlockHash();
     }
 
     public String getPreviousHash() {
         // Implementation
+//    	this.hashhistory.get(index);
+    	return this.previousHash;
     }
 
     public void setPreviousHash(String previousHash) {
         // Implementation
+    	this.previousHash = new String(previousHash);
     }
 
     public int getNonce() {
         // Implementation
+    	return this.nonce;
+    	
     }
 
     public void setNonce(int nonce) {
         // Implementation
+    	this.nonce = nonce;
     }
-
     @Override
     public String toString() {
-        // Implementation
+    	String outstr = "[Time: %d] Node Hash: %s with Nonce: %d ";
+    	String result = String.format(outstr,this.timestamp,this.hash,this.nonce);
+    	return result;
     }
-    
-    
-
-    public static void main(String[] args) {
-        // Main method implementation as provided in the original instructions
-    }
-    
-    
+      
 }
